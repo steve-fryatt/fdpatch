4fb801a21fb2427a3fd56c99b64b0a22a6ce6375
Steve Fryatt <info@stevefryatt.org.uk>
2020-10-25 21:36:39 +0000

    Update Asm SWI header name.

--- src/FDPatch.s
+++ src/FDPatch.s
@@ -26,7 +26,7 @@
 ;
 ; REM 26/32 bit neutral
 
-	GET	$Include/SWINames
+	GET	$Include/AsmSWINames
 
 ; ---------------------------------------------------------------------------------------------------------------------
 ; Set up the Module Workspace
